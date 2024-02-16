820d553783356ffcb3b045eabb91966b56db7ce0
Danielpdsr <dpetter283@gmail.com>
2024-02-16 16:49:31 -0300

    AULA SOBRE ARRAYS E FUNÇOES

---
+++ index.js
@@ -0,0 +1,48 @@
+/*var nome = prompt("Escreva seu nome");
+var cor = prompt("Escreva sua cor favorita")
+
+var Frase = "A cor favorita de " + nome + " é " + cor + ",";
+
+console.log(Frase)*/
+
+/*const Frase = prompt("Escreva uma frase");
+const FraseMaiuscula = Frase.toUpperCase()
+
+console.log(FraseMaiuscula)*/
+
+/*const Frase = prompt("Escreva uma frase");
+const novaFrase = frase.replaceAll("o","i")
+console.log =(Frase.toUpperCase)
+console.log(frase.FraseMaiuscula)
+console.log(frase.length)
+
+const listaDeCompras = ["abacate","Banana","Tomate"]
+const segundoItem = listaDeCompras[2]// Tomate*/
+
+
+//const listaDeCachorro = ["Viralata","Dalmata","Pitbull","pintcher","sabujo"]
+//const numero = prompt("Digite um número de 0 a 4")
+//console.log(listaDeCachorro[numero])
+
+/*console,log(cachoorro[prompt("Digite um numero de 0 a 4")])*/
+
+
+/*var array = ["a","b","c","d","e","f","g","h"];
+//          [0,   1,  2,  3,  4,  5,  6,  7 ]
+array.splice(3,3)
+console.log(array)*/
+
+/*var array = ["1","2","3","4","5","6"]
+console.log(array.length)
+console.log(array.push[7])
+console.log(array.splice(3,2))
+console.log(array.length)*/
+
+/*function imprimirOlaMundo() {
+    console.log("Ola Mundo!")
+} */
+
+function  imprimaNome(nome) {
+    console.log(`Ola ${nome}`)
+}
+imprimaNome('Daniel')
